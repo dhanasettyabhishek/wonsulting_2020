e7c5146054f731e3f51e4a6a3040ba266f34a733
dhanasettyabhishek <dhanasettyabhishek@gmail.com>
2020-08-16 01:49:40 -0500

    1:49 AM 8/16/2020

--- wonsulting_flask/application.py
+++ wonsulting_flask/application.py
@@ -1,11 +1,14 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 from markupsafe import escape
-from flask import render_template
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
 
+@app.route("/")
+# @app.route('/hello/')
+# @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
