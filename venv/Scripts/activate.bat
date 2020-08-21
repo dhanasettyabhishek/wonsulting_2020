5c6cd5da9d0c15f261a3299a3d36726128902ea8
dhanasettyabhishek <dhanasettyabhishek@gmail.com>
2020-08-20 21:45:09 -0500

    9:44PM 8/20/2020

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\OneDrive\OneDrive - University of Cincinnati\Wonsulting\wonsulting_2020\wonsulting_flask\venv
+set VIRTUAL_ENV=C:\OneDrive\OneDrive - University of Cincinnati\Wonsulting\wonsulting_2020\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
