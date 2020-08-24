5ff85d5b2e0e377af671fc656d661dc51241a494
Divijatippareddy <tippardy@gmail.com>
2020-08-24 02:10:56 -0500

    24-08-20 02:10

--- main.py
+++ main.py
@@ -118,13 +118,9 @@ def About():
 def Questions():
     return render_template('Questions.html')
 
-<<<<<<< HEAD:application.py
-=======
-
->>>>>>> e9e436dc878cacc9f1f5d25248cb94fcc45fe669:main.py
 @app.route('/credits')
 def credits():
-    return render_template("credits.html")
+    return render_template("Credits.html")
 
 
 set_food_id = set()
